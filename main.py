11a1fb4aef3eae0c2a7aab54d1b6a33b64c98b7f
CCOO_BOT <miqueljugi@gmail.com>
2025-05-17 12:08:45 +0200

    main.py

--- main.py
+++ main.py
@@ -70,4 +70,4 @@ def enviar_missatge_whatsapp(destinatari, missatge, phone_number_id):
    response = requests.post(url, headers=headers, json=data)
    print("Missatge enviat:", response.status_code, response.text)
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=10000)
+   app.run(host="0.0.0.0", port=5000)))
